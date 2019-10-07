ce2dc527c2cca1268be36d8721654f834b0ef05b
flux <25628292+fluxionary@users.noreply.github.com>
2019-10-07 18:33:55 +0000

    use computed relative pos (seems to be correct thing to do)

--- init.lua
+++ init.lua
@@ -8,15 +8,12 @@ unified_inventory.register_page("trashcan", {
     get_formspec = function(player, perplayer_formspec)
         local formspecy = perplayer_formspec.formspec_y
         local formspec = "background[0,"..(formspecy + 3.5)..";8,4;ui_main_inventory.png]"
-                      .. "background[3.56,2;0.92,0.92;ui_bags_trash.png]"
-                      .. "label[3.5,1.5;Trash:]"
-                      .. "list[detached:trash;main;3.5,2.1;1,1;]"
+                      .. "background[3.56,"..(formspecy + 1)..";0.92,0.92;ui_bags_trash.png]"
+                      .. "label[3.5,"..(formspecy + 0.5)..";Trash:]"
+                      .. "list[detached:trash;main;3.5,"..(formspecy + 1.1)..";1,1;]"
                       .. "listring[current_player;main]"
                       .. "listring[detached:trash;main]"
-        return {
-            formspec = formspec,
-
-        }
+        return {formspec = formspec}
     end
 })
 
